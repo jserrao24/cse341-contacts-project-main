05efd9acacc0670307b0c793b79adb9bbde0b6f4
Jared Serrao <jared.serrao5@gmail.com>
2024-01-10 20:13:24 -1000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+router.use('/', require('./swagger'));
+router.get('/', (req, res) => { 
+    //#swagger.tags=['Hello world']
+    res.send('hello world');
+});
+router.use("/contacts", require('./contacts'));
+module.exports = router;
