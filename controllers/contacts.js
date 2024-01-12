790502747eb0367b29da746d5efa29f404145a44
Jared Serrao <jared.serrao5@gmail.com>
2024-01-12 11:58:01 -1000

    test

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -16,7 +16,7 @@ const getSingle = async (req, res) => {
     const contactId = new ObjectId(req.params.id);
     const result = await mongodb.getDatabase().db().collection('contacts').find({ _id: contactId });
     result.toArray().then((contacts) => {
-        res.setHeader('Contect-Type', 'application/json');
+        res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
 };
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader(
             'Access-Control-Allow-Headers',
-            'Origin, X-requested-With, Content-Type, Accept, Z-Key'
+            'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
         );
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         next();
